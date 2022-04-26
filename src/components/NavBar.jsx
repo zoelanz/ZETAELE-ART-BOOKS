5a02d5762c3d6b62fc607ff0548137d98764fa82
zoedesarrolloweb <zoe_lanz@hotmail.com>
2022-04-26 13:46:45 -0300

    Creando NavBar con React js

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,50 @@
+
+
+
+function NavBar(){
+
+    return (
+<header>
+    <h1>ZETAELE|ART BOOKS</h1>
+    <div classname="navegador">
+        <nav>
+        <ul>
+            <li>
+            <a href="">TODOS LOS TíTULOS</a>
+            </li>
+            <li>
+            <a href="">MODA</a>
+            </li>
+            <li>
+            <a href="">FOTOGRAFíA</a>
+            </li>
+            <li>
+            <a href="">ARTE</a>
+            </li>
+            <li>
+            <a href="">ARQUITECTURA & DISEÑO</a>
+            </li>
+            {/* <li>
+            <i className=" iconoCart fa-brands fa-opencart" />
+            </li> */}
+        </ul>
+        <div id="containerMenu" className="barsHide iconos">
+            <i className="fa-solid fa-bars-staggered" aria-hideen="true" />
+            <i className="fa-brands fa-opencart " />
+        </div>
+        </nav>
+    </div>
+</header>
+        
+
+    
+       
+    )
+  }
+
+    
+    
+    
+
+export default NavBar
+
